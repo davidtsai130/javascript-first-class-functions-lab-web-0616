aab2c0eadbdbfe8eedd2e65e5d361a4b458e0917
davidtsai130 <davidtsai130@gmail.com>
2016-07-26 16:09:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function countdown(callback) {
-  window.setTimeout(callback(), 3000)
+  window.setTimeout(callback, 2000)
 }
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function createMultiplier(n) {
+  return function multiplierValue(value) {
+    return n * value
+  }
+}
+
+function multiplier(a, b) {
+  return a * b
+}
+
+var doublerWithBind = multiplier.bind(2)
+var triplerWithBind = multiplier.bind(3)
+
+      
